b97b921141c79e03b499dad1153daea4758aefa8
zakkuns <zkuns@mail.csuchico.edu>
2018-02-13 11:17:58 -0800

    Asks the user for their first and last name, then gives the user their nickname, and then gives the user their lucky number

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 
         string firstname;
         string lastname;
-
+        int lucky;
 
 
         //get user input
@@ -22,10 +22,11 @@ int main()
 
 
         //tell fortune
-
+        
+        lucky = firstname.length();
 
         cout << "Welcome, " << firstname[0] << "." << lastname[0] << "," << " here is your fortune...\n";    
-
+        cout << "Your lucky number is " << lucky << "." << endl;
 
         return 0;
 }
