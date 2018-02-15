4f17d588e201144c1e07734381149bbcd0ca7ce6
zakkuns <zkuns@mail.csuchico.edu>
2018-02-15 11:38:17 -0800

    Checks if the first letter of the users first name is a vowel and same for the last letter of their lastname

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main()
         string lastname;
         int lucky;
 
-
         //get user input
         cout << "Enter your first name.\n";
         cin >> firstname;
@@ -21,12 +20,46 @@ int main()
         cin >> lastname;
 
 
-        //tell fortune
+        int length = lastname.length();
+        char letter = lastname[length - 1];
         
+
+
+        //tell fortune
+
         lucky = firstname.length();
+
 
         cout << "Welcome, " << firstname[0] << "." << lastname[0] << "," << " here is your fortune...\n";    
         cout << "Your lucky number is " << lucky << "." << endl;
 
+
+
+
+        if (firstname[0] == 'a' || firstname[0] == 'e' || firstname[0] == 'i' || firstname[0] == 'o' || firstname[0] == 'u'
+                        || firstname[0] == 'A' || firstname[0] == 'E' || firstname[0] == 'I' || firstname[0] == 'O' || firstname[0] == 'U')
+
+        {
+                cout << "You are destined to be famous" << endl;
+
+        }
+
+
+
+
+
+
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'E' ||
+                        letter == 'I' || letter == 'O' || letter == 'U') 
+
+        {
+
+                cout << "You have already met your true love" << endl;
+
+        }
+
+        cout << "Have a good day!" << endl;
+        
+        
         return 0;
 }
